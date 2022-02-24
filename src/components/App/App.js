0405b893d7a9c628bcefa9b158c0f6658f929fe4
Mykola-Veres <veresnickolay@gmail.com>
2022-02-24 23:18:54 +0200

    add-redux-persist

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,3 @@
-// import { useEffect, useState } from 'react';
 import { ArticleConteiner } from './App.styled';
 import ContactForm from '../ContactForm';
 import Filter from '../ContactsFilter';
@@ -9,42 +8,27 @@ import {addContacts, filterContacts, removeContacts} from "../../redux/sliceCont
 
 export default function App () {
   const contacts = useSelector(state => state.contacts.items);
-  const dispatch = useDispatch()
-  console.log(contacts)
-  // const [contacts, setContacts] = useState(() => {return JSON.parse(window.localStorage.getItem('contacts')) ?? ""})
-  // const [filter, setFilter] = useState("")
+  const dispatch = useDispatch();
   const filter = useSelector(state => state.contacts.filter);
   const dispatchFilter = useDispatch();
-  console.log(filter)
-
-  // useEffect(() => {
-  //   window.localStorage.setItem("contacts", JSON.stringify(contacts))
-  // }, [contacts])
 
   const handlerSubmitUserForm = contact => {
     contacts.some(contactItem =>
         contactItem.name.toLocaleLowerCase() === contact.name.toLocaleLowerCase())
       ? alert(`${contact.name} is already in contacts`)
       : dispatch(addContacts(contact))
-      // setContacts(( prevContacts => [...prevContacts, contact]));
     resetFilter()
   };
 
-  const handlerFilterName = e => {dispatchFilter(filterContacts(e.target.value))
-        // setFilter(e.target.value)
-      };
+  const handlerFilterName = e => {dispatchFilter(filterContacts(e.target.value))};
 
   const filterVisibleContacts = () => contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
 
-  const handlerDeleteContact = name => {
-    dispatch(removeContacts(contacts.filter(contact => contact.name !== name)))
-    // setContacts (prevContacts => prevContacts.filter(contact => contact.name !== name))
-  };
+  const handlerDeleteContact = name => {dispatch(removeContacts(contacts.filter(contact => contact.name !== name)))};
 
   console.log('filterVisibleContacts()', filterVisibleContacts())
 
   const resetFilter = () => {dispatchFilter(filterContacts(""))}
-  // setFilter('');
 
     return (
       <ArticleConteiner>
