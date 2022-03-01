0e195533314aa4dace51b3bb4f3c7a85587b1cac
Mykola-Veres <veresnickolay@gmail.com>
2022-03-01 18:22:49 +0200

    change-app_add-redux

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,44 +2,15 @@ import { ArticleConteiner } from './App.styled';
 import ContactForm from '../ContactForm';
 import Filter from '../ContactsFilter';
 import ContactList from '../ContactList';
-import { useSelector, useDispatch } from 'react-redux';
-import {addContacts, filterContacts, removeContacts} from "../../redux/sliceContacts";
-
 
 export default function App () {
-  const contacts = useSelector(state => state.contacts.items);
-  const dispatch = useDispatch();
-  const filter = useSelector(state => state.contacts.filter);
-  const dispatchFilter = useDispatch();
-
-  const handlerSubmitUserForm = contact => {
-    contacts.some(contactItem =>
-        contactItem.name.toLocaleLowerCase() === contact.name.toLocaleLowerCase())
-      ? alert(`${contact.name} is already in contacts`)
-      : dispatch(addContacts(contact))
-    resetFilter()
-  };
-
-  const handlerFilterName = e => {dispatchFilter(filterContacts(e.target.value))};
-
-  const filterVisibleContacts = () => contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
-
-  const handlerDeleteContact = name => {dispatch(removeContacts(contacts.filter(contact => contact.name !== name)))};
-
-  console.log('filterVisibleContacts()', filterVisibleContacts())
-
-  const resetFilter = () => {dispatchFilter(filterContacts(""))}
-
     return (
       <ArticleConteiner>
         <h1>Phonebook</h1>
-        <ContactForm onSubmit={handlerSubmitUserForm} />
+        <ContactForm />
         <h2>Contacts</h2>
-        <Filter value={filter} onChange={handlerFilterName} />
-        <ContactList
-          contacts={filterVisibleContacts()}
-          onDeleteContact={handlerDeleteContact}
-        />
+        <Filter />
+        <ContactList />
       </ArticleConteiner>
     );
 }
